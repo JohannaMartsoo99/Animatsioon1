1a08669fd24dac7c8e88f36b7306735157f7e2df
JohannaM <johanna.martsootptlive.ee>
2018-06-07 09:04:20 +0300

    Analytics
    Lisatud analytics platvorm

--- Animation/MainActivity.cs
+++ Animation/MainActivity.cs
@@ -3,6 +3,10 @@ using Android.Widget;
 using Android.OS;
 using Android.Graphics.Drawables;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 namespace Animation
 {
     [Activity(Label = "Animation", MainLauncher = true)]
@@ -11,6 +15,10 @@ namespace Animation
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
+            AppCenter.Start("810a9973-d125-4cb2-9074-46a180d95902",
+                   typeof(Analytics), typeof(Crashes));
+            AppCenter.Start("810a9973-d125-4cb2-9074-46a180d95902", typeof(Analytics), typeof(Crashes));
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
